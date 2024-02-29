99402dc54473fe79c298d6fd9bb1a9f067563d06
Pennington <alexandra.pennington@acf.hhs.gov>
2024-02-29 13:23:46 -0500

    correct syntax error maybe?3

--- scripts/run-owasp-scan.sh
+++ scripts/run-owasp-scan.sh
@@ -30,4 +30,4 @@ CMD="python manage.py process_owasp_scan ${CMD_ARGS[@]} && echo 'goodbye'"
 echo $CMD
 
 # Submit a CF Task for execution that will run the necessary command
-cf7-cli run-task tdp-backend-staging --command "$CMD" --name nightly-owasp-scan
+cf run-task tdp-backend-staging --command "$CMD" --name nightly-owasp-scan
